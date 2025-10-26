eda2d30101a69d3358ce7ac73429624f3a48395b
YaGi4 <ebahtin6@gmail.com>
2025-10-26 17:12:13 +0300

    Fix test.js code for pipeline

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,4 @@
-const { add, multiply, greet } = require('../src/app');
-describe('Math functions', () => {
+const { add, multiply, greet, subtract, divide, power } = require('../src/app');describe('Math functions', () => {
  test('adds 2 + 3 to equal 5', () => {
  expect(add(2, 3)).toBe(5);
  });
