0e6163a494a0db04e1c9069479f936e0e1737c4b
YaGi4 <ebahtin6@gmail.com>
2025-10-26 16:09:21 +0300

    Complete power function implementation

--- src/app.js
+++ src/app.js
@@ -21,4 +21,10 @@ function divide(a, b) {
  }
  return a / b;
 }
-module.exports = { add, multiply, greet, subtract, divide };
+module.exports = { add, multiply, greet, subtract, divide, power };
+
+function power(base, exponent) {
+}
+function power(base, exponent) {
+ return Math.pow(base, exponent);
+}
